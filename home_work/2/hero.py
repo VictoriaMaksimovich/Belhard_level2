f096a96d4736609273bb3df4831d846bb4470070
VictoriaMaksimovich <853smile@gmail.com>
2024-10-20 01:51:51 +0300

    Add files via upload
    Изменены методы

--- home_work/2/hero.py
+++ home_work/2/hero.py
@@ -33,48 +33,27 @@ class Hero:
         self.strong = strong
 
     def print_info(self):
-        print(f'{'Name:'}{self.name} {'Health:'}{self.health} {'Armor:'}{self.armor} {'Strong:'}{self.strong}')
+        print(f'Name:{self.name} Health:{self.health} Armor:{self.armor} Strong:{self.strong}')
 
-    def kick(self, enemy, kick_k=1):
-        self.health -= kick_k
-        self.armor = self.armor - enemy.strong + kick_k
-        if self.armor < 0:
-            self.armor = 0
-        if self.health <= 0:
-            print(f'Hero {self.name} is dead')
+    def kick(self, enemy: 'Hero', kick_k=1):
+        print(f'{enemy.name} attacks!')
+        if self.armor > 0:
+            self.armor -= enemy.strong * kick_k
+            if self.armor < 0:
+                self.health += self.armor
         else:
-            print(f'Hero after kick: {'Name:'}{self.name} {'Health:'}{self.health} {'Armor:'}{self.armor} {'Strong:'}{self.strong}')
+            self.health -= enemy.strong * kick_k
+        print(f'{self.name} was attacked. Health:{self.health}')
 
-    def fight(self, enemy):
-        while True:
-            random_kick = random.choice([1, 2])
-            if random_kick == 1:
-                print(f'{enemy.name} attacks')
-                if self.armor > 0:
-                    self.armor -= (enemy.strong * 0.75)
-                    if self.armor > 0:
-                        self.health -= (enemy.strong * 0.25)
-                    else:
-                        self.health -= (enemy.strong * 0.25 - self.armor)
-                else:
-                    self.health -= enemy.strong
-                print(f'{self.name} health {round(self.health)}')
-            if random_kick == 2:
-                print(f'{self.name} attacks')
-                if enemy.armor > 0:
-                    enemy.armor -= (self.strong * 0.75)
-                    if enemy.armor > 0:
-                        enemy.health -= (self.strong * 0.25)
-                    else:
-                        enemy.health -= (self.strong * 0.25 - enemy.strong)
-                else:
-                    enemy.health -= self.strong
-                print(f'{enemy.name} health {round(enemy.health)}')
+    def fight(self, enemy: 'Hero'):
+        while self.health or enemy.health > 0:
+            self.kick(enemy)
+            enemy.kick(self)
             if self.health <= 0:
-                print(f'Hero {self.name} is dead')
+                print(f'{self.name} is dead')
                 break
             elif enemy.health <= 0:
-                print(f'Hero {enemy.name} is dead')
+                print(f'{enemy.name} is dead')
                 break
 
     
@@ -85,3 +64,4 @@ knight.print_info()
 witch.print_info()
 knight.kick(witch, 2)
 witch.fight(knight)
+
