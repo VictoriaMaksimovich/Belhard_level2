87c4e6ac66267e7c04495f38334e2d53255b8d18
VictoriaMaksimovich <853smile@gmail.com>
2024-11-28 17:29:31 +0300

    Add files via upload

--- home_work/university/info/admin.py
+++ home_work/university/info/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from .models import Student, Teacher, Course
+from .models import Student, Course, Mark
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('langs', 'course_num', 'start_date', 'end_date', 'description')
+    search_fields = ('langs', 'course_num', 'start_date')
 
 
 @admin.register(Student)
 class StudentsAdmin(admin.ModelAdmin):
-    list_display = ('name', 'surname', 'sex', 'active', 'short_name')
-    search_fields = ('name', 'surname')
-    list_filter = ('sex',)
+    list_display = ('name', 'surname', 'sex', 'active', 'short_name', 'startDate')
+    search_fields = ('name', 'surname', 'startDate')
+    list_filter = ('sex', 'startDate')
 
     def short_name(self, obj):
         return f"{obj.surname} {obj.name[0]}."
@@ -14,12 +20,12 @@ class StudentsAdmin(admin.ModelAdmin):
     short_name.short_description = 'Короткое имя'
 
 
-@admin.register(Teacher)
-class TeacherAdmin(admin.ModelAdmin):
-    list_display = ('name', 'surname')
-    search_fields = ('name', 'surname', 'subject')
-    list_filter = ('subject',)
+@admin.register(Mark)
+class MarkAdmin(admin.ModelAdmin):
+    list_display = ('student', 'mark')
+    search_fields = ('student', 'mark')
+    list_filter = ('student',)
 
 
-admin.site.register(Course)
+
 
--- home_work/university/info/models.py
+++ home_work/university/info/models.py
@@ -1,35 +1,67 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.urls import reverse
 
 
 class Course(models.Model):
-    number = models.CharField('Курс', max_length=1, choices=[1, 2, 3, 4, 5])
+    langs = [
+        ('py', 'Python'),
+        ('js', 'JavaScript'),
+        ('c', 'C++'),
+        ('an', 'Android'),
+    ]
+    name = models.CharField(max_length=20, choices=langs)
+    course_num = models.SmallIntegerField(
+        default=1,
+        verbose_name="Номер курса",
+        validators=[MinValueValidator(1), MaxValueValidator(100)])
+    start_date = models.DateField('Начало курса')
+    end_date = models.DateField('Окончание курса')
+    description = models.TextField('Описание', blank=True)
 
     def __str__(self):
-        return f"{self.number}"
+        return f"{self.name} - {self.course_num}"
+
+    class Meta:
+        unique_together = [['name', 'course_num']]
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+        ordering = ['name', 'course_num']
 
 
 class Student(models.Model):
-    name = models.CharField('Имя', max_length=20)
-    surname = models.CharField('Фамилия', max_length=30)
-    sex = models.CharField(max_length=10, choices=[('m', 'Мужчина'), ('w', 'Женщина')])
-    active = models.BooleanField()
-    startDate = models.DateField(null=True)
-    student = models.ForeignKey(Course, on_delete=models.CASCADE)
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    name = models.CharField('Имя', max_length=20, null=False, blank=False)
+    surname = models.CharField('Фамилия', max_length=30, null=False, blank=False)
+    sex = models.CharField('Пол', max_length=10, choices=[('m', 'Мужчина'), ('w', 'Женщина')])
+    active = models.BooleanField('Активный')
+    age = models.SmallIntegerField('Возраст', validators=[MinValueValidator(18), MaxValueValidator(120)])
+    startDate = models.DateField('Дата поступления')
+    course = models.ManyToManyField(to='Course', verbose_name='Посещаемые курсы')
 
     def __str__(self):
         return f"{self.name} {self.surname}"
 
+    def get_absolute_url(self):
+        return reverse('student', kwargs={"id": self.pk})
 
-class Teacher(models.Model):
-    name = models.CharField('Имя', max_length=20)
-    surname = models.CharField('Фамилия', max_length=30)
-    subject = models.CharField('Предмет', max_length=20, choices=['Информатика', 'Математика',
-                                                                  'Физика', 'Химия', 'Биология', 'Физкультура'])
-    student = models.ManyToManyField(Student, through="Student")
+    class Meta:
+        indexes = [models.Index(fields=['surname'])]
+        verbose_name = "Студент"
+        verbose_name_plural = "Студенты"
+        # unique_together = [['name', 'surname']]
+        db_table = 'students'
+        ordering = ["surname"]
+
+
+class Mark(models.Model):
+    mark = models.SmallIntegerField('Оценка', validators=[MinValueValidator(1), MaxValueValidator(10)])
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.name} {self.surname}"
+        return f'{self.student} - {self.mark}'
 
-
+    class Meta:
+        indexes = [models.Index(fields=['student'])]
+        verbose_name = "Оценка"
+        verbose_name_plural = "Оценки"
 
